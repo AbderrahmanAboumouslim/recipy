ac2b636c9fce0e1a2a2a1dcb0057fd13b63a4972
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2021-11-04 23:01:55 +0100

    method 2 pagination

--- src/js/views/pagination.js
+++ src/js/views/pagination.js
@@ -1,60 +1,70 @@
-import icons from 'url:../../img/icons.svg'
-import view from './View';
+import icons from "url:../../img/icons.svg";
+import view from "./View";
 
 class Pagination extends view {
-    _parentElement = document.querySelector('.pagination');
+  _parentElement = document.querySelector(".pagination");
 
-    addHandlerPagination(handler){
-        this._parentElement.addEventListener('click', (e)=> {
-            const btn = e.target.closest('.btn--inline');
-            const attributePage = +btn.dataset.page;
-            handler(attributePage)
-        })
-    }
+  addHandlerPagination(handler) {
+    this._parentElement.addEventListener("click", (e) => {
+      const btn = e.target.closest(".btn--inline");
+      const attributePage = +btn.dataset.page;
+      handler(attributePage);
+    });
+  }
 
-    _generateMarkup(){
-        const numPages = Math.ceil(this._data.results.length / this._data.res_page);
-        const curPage = this._data.page;
+  _generateMarkup() {
+    const numPages = Math.ceil(this._data.results.length / this._data.res_page);
+    // const curPage = this._data.page;
 
-        // first page and still other(s) page(s)
-        if(curPage === 1 && numPages > curPage){
-            return `<button data-page="${curPage + 1}" class="btn--inline pagination__btn--next">
-                        <span>Page ${curPage + 1}</span>
-                        <svg class="search__icon">
-                            <use href="${icons}#icon-arrow-right"></use>
-                        </svg>
-                    </button>`
-        }
+    // ---------------method 2------------------------------
+    ////
+    const arr = Array.from({ length: numPages });
+    const test = arr.map((_, index) => {
+      return `<button class="btn--inline pagination__btn--next">${
+        index + 1
+      }</button>`;
+    });
+    return test;
+    /////
 
-        // last page
-        if(curPage === numPages && numPages > 1){
-            return `<button data-page="${curPage - 1}" class="btn--inline pagination__btn--prev">
-                        <svg class="search__icon">
-                            <use href="${icons}#icon-arrow-left"></use>
-                        </svg>
-                        <span>Page ${curPage -1}</span>
-                    </button>`
-        }
+    // ---------------method 1------------------------------
+    // // first page and still other(s) page(s)
+    // if(curPage === 1 && numPages > curPage){
+    //     return `<button data-page="${curPage + 1}" class="btn--inline pagination__btn--next">
+    //                 <span>Page ${curPage + 1}</span>
+    //                 <svg class="search__icon">
+    //                     <use href="${icons}#icon-arrow-right"></use>
+    //                 </svg>
+    //             </button>`
+    // }
 
-        // middle pages (not first page and not last page)
-        if(numPages > curPage){
-            return `<button data-page="${curPage - 1}" class="btn--inline pagination__btn--prev">
-                        <svg class="search__icon">
-                            <use href="${icons}#icon-arrow-left"></use>
-                        </svg>
-                        <span>Page ${curPage - 1}</span>
-                    </button>
-                    <button data-page="${curPage + 1}" class="btn--inline pagination__btn--next">
-                        <span>Page ${curPage + 1}</span>
-                        <svg class="search__icon">
-                            <use href="${icons}#icon-arrow-right"></use>
-                        </svg>
-                    </button>`
-        }
+    // // last page
+    // if(curPage === numPages && numPages > 1){
+    //     return `<button data-page="${curPage - 1}" class="btn--inline pagination__btn--prev">
+    //                 <svg class="search__icon">
+    //                     <use href="${icons}#icon-arrow-left"></use>
+    //                 </svg>
+    //                 <span>Page ${curPage -1}</span>
+    //             </button>`
+    // }
 
-        return ''
+    // // middle pages (not first page and not last page)
+    // if(numPages > curPage){
+    //     return `<button data-page="${curPage - 1}" class="btn--inline pagination__btn--prev">
+    //                 <svg class="search__icon">
+    //                     <use href="${icons}#icon-arrow-left"></use>
+    //                 </svg>
+    //                 <span>Page ${curPage - 1}</span>
+    //             </button>
+    //             <button data-page="${curPage + 1}" class="btn--inline pagination__btn--next">
+    //                 <span>Page ${curPage + 1}</span>
+    //                 <svg class="search__icon">
+    //                     <use href="${icons}#icon-arrow-right"></use>
+    //                 </svg>
+    //             </button>`
+    // }
 
-
-    }
+    // return ''
+  }
 }
 export default new Pagination();
